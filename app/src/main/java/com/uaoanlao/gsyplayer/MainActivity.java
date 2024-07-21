c798001f332596a1bab2225ed7515eb6ae7ebc7a
UaoanLao <59863314+uaoan@users.noreply.github.com>
2024-07-21 17:31:26 +0800

    1.0.2

--- app/src/main/java/com/uaoanlao/gsyplayer/MainActivity.java
+++ app/src/main/java/com/uaoanlao/gsyplayer/MainActivity.java
@@ -147,7 +147,7 @@ public class MainActivity extends AppCompatActivity {
 
         super.onDestroy();
 
-        GSYVideoManager.releaseAllVideos();
+        player.onVideoReleaseAllVideos();
 
 
 
